028e4754b722708859d7687be8e88b0b0052ea8c
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2020-11-16 17:27:56 +0100

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
+import SidebarChat from './SidebarChat';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://picsum.photos/id/1010/5184/3456" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="search or start new chat"/>
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar;
